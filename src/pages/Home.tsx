590b24487179b9b78de823fd08aee0588dc6522a
GoGeunWoo <gkw0314@naver.com>
2024-07-23 19:26:35 +0900

    [STYLE] : 포스트 스타일 생성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import Header from "@components/Header";
-import MainTitle from "@components/MainTitle";
+
 
 const Home = () => {
   return (
     <div>
-    <Header/>
-    <MainTitle />
+
   </div>
   )
 };
