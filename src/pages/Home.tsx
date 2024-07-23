12febe9a58292aa57b261431bb6cdbede801d262
summereuna <summereuna@gmail.com>
2024-07-23 13:40:01 +0900

    [STYLE] : 아이콘 정리

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import ClothesTag from "@components/ClothesTag";
 import ColorBar from "@components/ColorBar";
 import ColorChip from "@components/ColorChip";
+import Icon from "@components/Icon";
+import { heartFillIcon, heartIcon, menuIcon } from "@shared/icons";
 
 const Home = () => {
   return (
@@ -9,6 +11,9 @@ const Home = () => {
       <ColorBar></ColorBar>
       <ClothesTag color="red" type="티셔츠" />
       <ColorChip color={"red"} />
+      <Icon icon={menuIcon} />
+      <Icon icon={heartIcon} />
+      <Icon icon={heartFillIcon} />
     </div>
   );
 };
