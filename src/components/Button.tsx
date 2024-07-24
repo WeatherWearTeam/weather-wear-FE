a156d54c63755f617fccfa890cd5bd258c0a34f3
summereuna <summereuna@gmail.com>
2024-07-24 17:30:59 +0900

    [STYLE] : 버튼 컴포넌트

---
+++ src/components/Button.tsx
@@ -0,0 +1,85 @@
+import Icon from "@components/Icon";
+import styled from "styled-components";
+
+type buttonType = "primary" | "secondary";
+
+interface ButtonProps {
+  children: React.ReactNode;
+  icon?: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  buttonType?: buttonType;
+  type?: "button" | "submit";
+  htmlFor?: string;
+  selected?: boolean;
+}
+
+export default function Button({
+  children,
+  type = "button",
+  icon = null,
+  onClick,
+  disabled = false,
+  buttonType = "primary",
+  htmlFor,
+  selected = false,
+}: ButtonProps) {
+  return (
+    <StButton
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      $buttonType={buttonType}
+      id={htmlFor}
+      $selected={selected}
+    >
+      {icon && (
+        <Container>
+          <Icon icon={icon} />
+          {children}
+        </Container>
+      )}
+      {!icon && children}
+    </StButton>
+  );
+}
+
+interface buttonsProps {
+  $buttonType?: buttonType;
+  $selected?: boolean;
+}
+
+const StButton = styled.button<buttonsProps>`
+  background-color: ${({ theme, $buttonType = "primary" }) =>
+    theme.buttons[$buttonType].backgroundColor};
+  border: ${({ theme, $buttonType = "primary" }) =>
+    theme.buttons[$buttonType].border || "inherit"};
+  color: ${({ theme, $buttonType = "primary" }) =>
+    theme.buttons[$buttonType].color || "inherit"};
+  width: 100%;
+  padding: 1.2rem 0;
+  cursor: pointer;
+  transition: border-color linear 0.2s;
+  outline: none;
+
+  &:hover {
+    ${({ theme, $buttonType = "primary" }) => theme.buttons[$buttonType].hover};
+  }
+  &:focus {
+    ${({ theme, $buttonType = "primary" }) => theme.buttons[$buttonType].focus};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 1.5rem;
+  font-size: small;
+`;
