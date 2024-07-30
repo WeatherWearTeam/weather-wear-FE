b51eeab3ebeb963a26c4db475ebca1a06182d7f1
summereuna <summereuna@gmail.com>
2024-07-30 14:36:59 +0900

    [SET] : 데이터 목작업 위해 json-server 세팅

--- src/api/boardApi.ts
+++ src/api/boardApi.ts
@@ -2,14 +2,14 @@ import api from "@api/api";
 
 // 전체 Boards 리소스 조회
 export const getBoards = async () => {
-  const response = await api.get(`/board`);
+  const response = await api.get(`/boards`);
   return response.data;
 };
 
 // Board 리소스 생성
 // FormData를 사용하는 경우 타입도 폼 데이터
 export const createBoard = async (newBoardFormData: FormData) => {
-  const response = await api.post(`/board`, newBoardFormData, {
+  const response = await api.post(`/boards`, newBoardFormData, {
     headers: {
       "Content-Type": "multipart/form-data", // FormData를 사용하는 경우
     },
