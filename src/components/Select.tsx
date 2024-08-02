f79d847129bdc4a7987f67304474446c4c7d42b8
summereuna <summereuna@gmail.com>
2024-08-03 02:35:09 +0900

    [FEAT] : color bar, select 컴포넌트 타입 추가

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,12 +1,13 @@
+import { ClothesType } from "@store/clothesTagStore";
 import DropdownLayout from "@components/Modal/DropdownLayout";
 import ModalPortal from "@components/Modal/ModalPortal";
 import SelectButton from "@components/SelectButton";
 import useDropdownPosition from "@hooks/useDropdownPosition";
 import useModal from "@hooks/useModal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const clothesTypeList = [
+const clothesTypeList: OptionType[] = [
   { key: "SLEEVELESS", value: "민소매" },
   { key: "SHORT_SLEEVE", value: "반팔" },
   { key: "LIGHT_SHIRT", value: "얇은 셔츠" },
@@ -17,17 +18,17 @@ const clothesTypeList = [
   { key: "KNIT", value: "니트" },
   { key: "HOODIE", value: "후드" },
   { key: "SWEAT_SHIRT", value: "맨투맨" },
-
+  //////////////////////////////////////////////////
   { key: "SHORTS", value: "반바지" },
   { key: "MINI_SKIRT", value: "짧은 치마" },
   { key: "DRESS", value: "원피스" },
   { key: "SLACKS", value: "슬랙스" },
   { key: "COTTON_PANTS", value: "면바지" },
   { key: "JEANS", value: "청바지" },
-
+  //////////////////////////////////////////////////
   { key: "STOCKINGS", value: "스타킹" },
   { key: "LEGGINGS", value: "레깅스" },
-
+  //////////////////////////////////////////////////
   { key: "CARDIGAN", value: "가디건" },
   { key: "JACKET", value: "자켓" },
   { key: "TRENCH_COAT", value: "트렌치코트" },
@@ -35,44 +36,39 @@ const clothesTypeList = [
   { key: "LEATHER_JACKET", value: "가죽자켓" },
   { key: "COAT", value: "코트" },
   { key: "PADDED_COAT", value: "패딩" },
+  //////////////////////////////////////////////////
   { key: "HEAT_TECH", value: "히트텍" },
   { key: "LINED_CLOTHING", value: "기모" },
   { key: "SCARF", value: "목도리" },
 ];
 
-// const clothesTypeList = [
-//   "나시",
-//   "티셔츠",
-//   "셔츠/블라우스",
-//   "맨투맨/후드",
-//   "니트/스웨터",
-//   "반바지",
-//   "바지/청바지",
-//   "치마",
-//   "원피스",
-//   "가디건",
-//   "아우터",
-//   "더 추가 할 항목 있나요?",
-// ];
+type SelectValue = ClothesType | "옷 종류";
+
+interface OptionType {
+  key: string;
+  value: SelectValue;
+}
 
 interface SelectProps {
-  onClick: (clotheType: string) => void; //일단 옵셔널
-  value: string; //일단 옵셔널
-  // label?: string; //일단 옵셔널
-  // name?: string; //일단 옵셔널
+  onClick: (clothesType: ClothesType) => void;
+  value: SelectValue;
 }
 
 export default function Select({ onClick, value }: SelectProps) {
   const { openModal, closeModal, isVisible } = useModal();
-  const [selectedOption, setSelectedOption] = useState(value);
+  const [selectedOption, setSelectedOption] = useState<SelectValue>(value);
 
   const { dropdownPosition, divRef } = useDropdownPosition(isVisible);
 
-  const onSelectClick = (clotheType: string) => {
-    setSelectedOption(clotheType);
-    onClick && onClick(clotheType);
+  const onSelectClick = (clothesType: ClothesType) => {
+    setSelectedOption(clothesType);
+    onClick && onClick(clothesType);
     closeModal();
   };
+
+  useEffect(() => {
+    setSelectedOption(value); // value prop이 변경되면 selectedOption을 업데이트
+  }, [value]);
 
   return (
     <div>
