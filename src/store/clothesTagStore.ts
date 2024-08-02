d7e45e2cbb7e9a068452a63af2edd3f8bc274f4a
summereuna <summereuna@gmail.com>
2024-08-03 02:37:10 +0900

    [FEAT] : zustand clothesTagSotre에 isSingleTag 상태 추가하여 add페이지별 다르게 사용하도록 설정

--- src/store/clothesTagStore.ts
+++ src/store/clothesTagStore.ts
@@ -57,11 +57,13 @@ interface StoreState {
   selectedType: ClothesType | "옷 종류";
   selectedColor: ClothesColorType | "";
   newTagList: ClothesTag[];
+  isSingleTag: boolean; // 게시판에 따라 단일 태그 추가 여부
 }
 
 interface StoreActions {
   setSelectedType: (type: ClothesType | "옷 종류") => void;
   setSelectedColor: (color: ClothesColorType | "") => void;
+  setIsSingleTag: (isSingle: boolean) => void;
 
   addTag: (tag: ClothesTag) => void;
   removeTag: (id: number) => void;
@@ -73,17 +75,29 @@ const useClothesTagStore = create<StoreState & StoreActions>((set) => ({
   selectedType: "옷 종류",
   selectedColor: "",
   newTagList: [],
+  isSingleTag: true, // 기본값 설정
 
   setSelectedType: (type) => set({ selectedType: type }),
   setSelectedColor: (color) => set({ selectedColor: color }),
+  setIsSingleTag: (isSingle) => set({ isSingleTag: isSingle }),
 
   addTag: (tag) =>
     set((state) => {
-      // '옷 종류' 상태일 때는 태그 추가를 방지
+      // "옷 종류" 상태일 때는 태그 추가를 방지
       if (state.selectedType === "옷 종류" || state.selectedColor === "") {
         return state; // 상태를 변경하지 않음
       }
 
+      // 태그 1개만 가능 => Clothes 추가
+      if (state.isSingleTag) {
+        return {
+          newTagList: [tag],
+          selectedType: "옷 종류", // 리셋
+          selectedColor: "",
+        };
+      }
+
+      // 태그 여러개 => Board 추가
       return {
         newTagList: [...state.newTagList, tag],
         selectedType: "옷 종류", // 리셋
