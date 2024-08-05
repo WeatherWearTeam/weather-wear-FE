8dca48c18c5367184de371d39e5af904a9098d28
summereuna <summereuna@gmail.com>
2024-08-05 10:53:27 +0900

    [FEAT] : 라우터 추가

--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "@pages/Home";
 import Layout from "@shared/Layout";
 import MyPage from "@pages/MyPage";
@@ -13,6 +13,29 @@ import Login from "@pages/Login";
 import Signup from "@pages/Signup";
 import MyAccount from "@pages/MyAccount";
 import MyAccountEdit from "@pages/MyAccountEdit";
+import PostEdit from "@pages/PostEdit";
+import useAuth from "@queries/useAuth";
+
+// PrivateRoute : 로그인이 필요한 페이지에 접근할 수 있도록 하는 컴포넌트
+// 로그인이 되어있지 않은 사용자는 login 페이지로 리다이렉트
+const PrivateRoute: React.FC<{ element: React.ElementType }> = ({
+  element: Element,
+  ...rest
+}) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? <Element {...rest} /> : <Navigate to="/login" />;
+};
+
+// PublicRoute : 사용자가 로그인하지 않았을 때만 렌더링을 허용
+// 로그인한 사용자는 홈 페이지로 리다이렉트
+// 로그인이 되어있는 사용자는 /로 리다이렉트
+const PublicRoute: React.FC<{ element: React.ElementType }> = ({
+  element: Element,
+  ...rest
+}) => {
+  const { isLoggedIn } = useAuth();
+  return !isLoggedIn ? <Element {...rest} /> : <Navigate to="/" />;
+};
 
 const Router = () => {
   return (
@@ -34,10 +57,12 @@ const Router = () => {
           </Route>
 
           <Route path="/mypage/closet/add" element={<ClosetAdd />} />
+          <Route path="/mypage/closet/:id/edit" element={<ClosetEdit />} />
 
           <Route path="/ootd" element={<Trend />} />
 
           <Route path="/ootd/add" element={<PostAdd />} />
+          <Route path="/ootd/:id/edit" element={<PostEdit />} />
 
           <Route path="/ootd/:id" element={<PostDetail />} />
         </Routes>
