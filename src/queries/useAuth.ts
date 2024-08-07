a03dfa504fab88cf0f82392975512254b2625bbe
summereuna <summereuna@gmail.com>
2024-08-08 01:43:38 +0900

    [FEAT] : my account 수정하는 formData 로직 수정

--- src/queries/useAuth.ts
+++ src/queries/useAuth.ts
@@ -1,6 +1,5 @@
 import api from "@api/api";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 
 //로그인
@@ -30,7 +29,6 @@ const logout = async () => {
 const useAuth = () => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
-  // const [cookies, , removeCookie] = useCookies(["Authorization"]);
 
   const {
     mutate: mutateLogin,
@@ -45,15 +43,6 @@ const useAuth = () => {
       }
       queryClient.invalidateQueries({ queryKey: ["auth"] });
       navigate("/"); //페이지 이동
-      // 서버에서 HttpOnly 쿠키를 설정해주므로 클라이언트에서 쿠키를 설정할 필요가 없음
-      //     setCookie("accessToken", accessToken, {
-      //       path: "/",
-      //       secure: true,
-      //       sameSite: "lax",
-      //       maxAge: 3600, //1시간 (s)
-      //       //만료 시간을 UTC 기준으로 표시하기 때문에 KST 보다 -9시간이긴 하지만 기능상 문제는 없음
-      //     });
-      //   }
     },
   });
 
@@ -64,7 +53,6 @@ const useAuth = () => {
   } = useMutation({
     mutationFn: logout,
     onSuccess: () => {
-      // removeCookie("Authorization");
       localStorage.removeItem("ISLOGGEDIN");
       // queryClient.invalidateQueries({ queryKey: ["auth"] }); // 이거지우니까 에러 안뜸 //없는데 호출해서 그런거였음
       navigate("/"); //페이지 이동
