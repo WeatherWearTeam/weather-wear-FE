4697f25b1cdcac51b7057f08e68ae6dcbf3dc67d
summereuna <summereuna@gmail.com>
2024-08-12 16:02:19 +0900

    [FIX] : 스타일컴포넌트에 변수 추가시 $추가

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -13,11 +13,10 @@ interface Props {
 
 export default function Pagination({
   totalPages,
-  pageCount, //5페이지씩 보여주기 =5
+  pageCount,
   currentPage,
   onPageChange,
 }: Props) {
-  // const totalPages = Math.ceil(totalItems / itemCountPerPage);
   const [start, setStart] = useState(1);
   const noPrev = start === 1;
   const noNext = start + pageCount - 1 >= totalPages;
@@ -30,7 +29,7 @@ export default function Pagination({
   return (
     <Wrapper>
       <List>
-        <Move invisible={noPrev}>
+        <Move $invisible={noPrev}>
           <LinkDivButton
             to={`?page=${start - 1}`}
             onClick={() => onPageChange(start - 1)}
@@ -40,10 +39,10 @@ export default function Pagination({
             </PreviousButton>
           </LinkDivButton>
         </Move>
-        {[...Array(pageCount)].map((_, i) => (
-          <>
-            {start + i <= totalPages && (
-              <Page key={i} active={currentPage === start + i}>
+        {[...Array(pageCount)].map(
+          (_, i) =>
+            start + i <= totalPages && (
+              <Page key={start + i} $active={currentPage === start + i}>
                 <LinkDiv
                   to={`?page=${start + i}`}
                   onClick={() => onPageChange(start + i)}
@@ -51,10 +50,9 @@ export default function Pagination({
                   {start + i}
                 </LinkDiv>
               </Page>
-            )}
-          </>
-        ))}
-        <Move invisible={noNext}>
+            )
+        )}
+        <Move $invisible={noNext}>
           <LinkDivButton
             to={`?page=${start + pageCount}`}
             onClick={() => onPageChange(start + pageCount)}
@@ -95,32 +93,22 @@ const ListItem = styled.li`
   justify-content: center;
 `;
 
-const Move = styled(ListItem)<{ invisible: boolean }>`
+const Move = styled(ListItem)<{ $invisible?: boolean }>`
   position: relative;
   cursor: pointer;
   margin: 0 10px;
-  visibility: ${({ invisible }) => (invisible ? "hidden" : "visible")};
-
-  /* a {
-    width: 50px;
-    display: block;
-    z-index: 10;
-    text-decoration: none;
-    &:hover {
-      text-decoration: underline;
-    }
-  } */
+  visibility: ${({ $invisible }) => ($invisible ? "hidden" : "visible")};
 `;
 
-const Page = styled(ListItem)<{ active: boolean }>`
+const Page = styled(ListItem)<{ $active: boolean }>`
   margin: 0 5px;
   cursor: pointer;
   width: 4rem;
   border-radius: 30px;
   text-align: center;
-  font-weight: ${({ active }) => (active ? "700" : "normal")};
-  background: ${({ active }) => (active ? "black" : "none")};
-  color: ${({ active }) => (active ? "white" : "inherit")};
+  font-weight: ${({ $active }) => ($active ? "700" : "normal")};
+  background: ${({ $active }) => ($active ? "black" : "none")};
+  color: ${({ $active }) => ($active ? "white" : "inherit")};
   border: 1px solid transparent;
   transition: border 0.15s linear;
   &:hover {
